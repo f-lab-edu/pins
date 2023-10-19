46d8f56871afb920a413fd55a5cfbb2bac5f4aca
MojitoBar <wnehdtjr5@gmail.com>
2023-10-19 15:08:14 +0900

    project init

---
+++ pins/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pins
+//
+//  Created by 주동석 on 2023/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
