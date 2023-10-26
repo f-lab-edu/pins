5dcf6ab13a6abd62cb3813092bab9bc6aa6c7c56
MojitoBar <wnehdtjr5@gmail.com>
2023-10-26 13:44:58 +0900

    os log extension add

---
+++ pins/Extension/OSLog.swift
@@ -0,0 +1,13 @@
+//
+//  Log.swift
+//  pins
+//
+//  Created by 주동석 on 2023/10/26.
+//
+
+import OSLog
+
+extension OSLog {
+  private static var subsystem = Bundle.main.bundleIdentifier!
+  static let ui = OSLog(subsystem: subsystem, category: "UI")
+}
