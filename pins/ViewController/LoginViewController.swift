5081eb163e78978d2bda1c0d65d4abb5bb8d87a0
MojitoBar <wnehdtjr5@gmail.com>
2023-11-03 15:07:59 +0900

    [#12] firebase apple login added

--- pins/ViewController/LoginViewController.swift
+++ pins/ViewController/LoginViewController.swift
@@ -6,9 +6,14 @@
 //
 
 import UIKit
+import OSLog
 import AuthenticationServices
+import CryptoKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
+    fileprivate var currentNonce: String?
+
     var loginView: LoginView {
         view as! LoginView
     }
@@ -23,27 +28,66 @@ class LoginViewController: UIViewController {
     }
     
     private func setAction() {
-        loginView.setLoginAction(UIAction(handler: { _ in
-            let request = ASAuthorizationAppleIDProvider().createRequest()
-            print(ASAuthorizationAppleIDProvider().getCredentialState(forUserID: UserDefaults.standard.string(forKey: "UserId") ?? "") { (credential, error) in
-                switch credential {
-                case .authorized:
-                    print("authorized")
-                case .revoked:
-                    print("revoked")
-                case .notFound:
-                    print("notFound")
-                default:
-                    break
-                }
-            })
-            request.requestedScopes = [.fullName, .email]
-            
-            let controller = ASAuthorizationController(authorizationRequests: [request])
-            controller.delegate = self
-            controller.presentationContextProvider = self
-            controller.performRequests()
+        loginView.setLoginAction(UIAction(handler: { [weak self] _ in
+            self?.startSignInWithAppleFlow()
         }))
+    }
+    
+    func startSignInWithAppleFlow() {
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            return String(format: "%02x", $0)
+        }.joined()
+        
+        return hashString
+    }
+    
+    // Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: Array<Character> =
+            Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+            
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        
+        return result
     }
 }
 
@@ -53,16 +97,34 @@ extension LoginViewController: ASAuthorizationControllerDelegate, ASAuthorizatio
     }
     
     func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
-        guard let credential = authorization.credential as? ASAuthorizationAppleIDCredential else { return }
-        UserDefaults.standard.setValue(credential.user, forKey: "UserId")
-        credential.
-        // 처음 애플 로그인 시 이메일은 credential.fullName 에 들어있다.
-        if let fullName = credential.fullName {
-            print("이름 : \(fullName.familyName ?? "")\(fullName.givenName ?? "")")
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            guard let nonce = currentNonce else {
+                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+            }
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                os_log("Unable to fetch identity token", log: .ui, type: .error)
+                return
+            }
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                os_log("Unable to serialize token string from data: %@", log: .ui, type: .error, appleIDToken.debugDescription)
+                return
+            }
+            
+            let credential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idTokenString, rawNonce: nonce)
+            
+            Auth.auth().signIn(with: credential) { [weak self] authResult, error in
+                if let error = error {
+                    os_log ("Error Apple sign in: %@", log: .ui, type: .error, error.localizedDescription)
+                    return
+                }
+                
+                let mainView: MainViewController = MainViewController()
+                self?.navigationController?.pushViewController(mainView, animated: true)
+            }
         }
     }
     
     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
-        print("애플 로그인 실패 \(error)")
+        os_log("애플 로그인에 실패했습니다.", log: OSLog.ui, type: .fault)
     }
 }
