eb51089677f1bcdf13416afcb2150a87e2ecaebc
MojitoBar <wnehdtjr5@gmail.com>
2023-12-10 18:58:57 +0900

    MainViewModel 테스트 코드 수정

--- pinsTests/MainViewModelTests.swift
+++ pinsTests/MainViewModelTests.swift
@@ -8,61 +8,128 @@
 import XCTest
 @testable import pins
 
-final class MainViewModelTests: XCTestCase {
-    var viewModel: MainViewModel!
-    var mockMainUseCase: MockMainUseCase!
-
-    override func setUp() {
-        super.setUp()
-        mockMainUseCase = MockMainUseCase()
-        viewModel = MainViewModel(mainUseCase: mockMainUseCase)
+class MockUserService: UserServiceProtocol {
+    func putUser(user: pins.UserRequest) {
+        fatalError("Not implemented")
     }
     
-    func testGetPins() async {
-        let expectation = XCTestExpectation(description: "getPins completes")
-        let expectedPins: [PinRequest] = [
-            PinRequest(id: "1", title: "title", content: "content", longitude: 1, latitude: 2, category: "category", created: "created")
-        ]
-        mockMainUseCase.pinsToReturn = expectedPins
+    var mockUser: UserRequest = UserRequest(id: "testId", nickName: "testNickname", firstTime: false, profileImage: "testImage")
 
-        viewModel.getPins()
-        
-        var receivedPins: [PinRequest] = []
-        let cancellable = viewModel.$currentPins.sink { pins in
-            receivedPins = pins
-            expectation.fulfill()
-        }
-
-        await fulfillment(of: [expectation], timeout: 1)
-
-        XCTAssertEqual(receivedPins, expectedPins)
-
-        cancellable.cancel()
-    }
-
-    func testSetCreateViewIsPresented() {
-        // setCreateViewIsPresented 메소드 실행
-        viewModel.setCreateViewIsPresented(isPresented: true)
-
-        // 결과 검증
-        XCTAssertTrue(viewModel.getCreateViewIsPresented(), "setCreateViewIsPresented 메소드가 상태를 올바르게 설정하지 못함")
-    }
-
-    func testToggleCreateViewIsPresented() {
-        // 처음 상태를 false로 설정
-        viewModel.setCreateViewIsPresented(isPresented: false)
-        // toggleCreateViewIsPresented 메소드 실행
-        viewModel.toggleCreateViewIsPresented()
-
-        // 결과 검증
-        XCTAssertTrue(viewModel.getCreateViewIsPresented(), "toggleCreateViewIsPresented 메소드가 상태를 올바르게 토글하지 못함")
+    func getUser(id: String) async -> UserRequest? {
+        return mockUser
     }
 }
 
-class MockMainUseCase: MainUseCaseProtocol {
-    var pinsToReturn: [PinRequest] = []
+class MockFirestorageService: FirestorageServiceProtocol {
+    func uploadImage(imageInfo: pins.ImageInfo) async -> pins.URLWithIndex {
+        fatalError("Not implemented")
+    }
+    
+    func uploadImages(imageInfos: [pins.ImageInfo]) async -> [pins.URLWithIndex] {
+        fatalError("Not implemented")
+    }
+    
+    func createPin(pin: pins.PinRequest, images: [pins.ImageInfo]) async {
+        fatalError("Not implemented")
+    }
+    
+    var mockPins: [PinRequest] = [PinRequest(id: "testId", title: "testTitle", content: "testContent", longitude: 0.0, latitude: 0.0, category: "testCategory", created: "testCreated", userId: "testUserId", urls: ["testUrl"])]
+    var mockImage: UIImage = UIImage() // 테스트를 위한 기본 이미지
 
     func getPins() async -> [PinRequest] {
-        return pinsToReturn
+        return mockPins
+    }
+
+    func downloadImage(urlString: String) async -> UIImage? {
+        return mockImage
+    }
+}
+
+final class MainViewModelTests: XCTestCase {
+    func testGetPins() async {
+        // Given
+        let mockFirestorageService = MockFirestorageService()
+        let mockUserService = MockUserService()
+        let useCase = MainUseCase(firestorageService: mockFirestorageService, userService: mockUserService)
+        let viewModel = MainViewModel(mainUseCase: useCase)
+        
+        // When
+        await viewModel.setCurrentPins()
+        
+        // Then
+        XCTAssertEqual(viewModel.currentPins, mockFirestorageService.mockPins, "getPins should return the pins from the mock service")
+    }
+    
+    func testLoadPin() async {
+        // Given
+        let mockFirestorageService = MockFirestorageService()
+        let mockUserService = MockUserService()
+        let useCase = MainUseCase(firestorageService: mockFirestorageService, userService: mockUserService)
+        let viewModel = MainViewModel(mainUseCase: useCase)
+        let pinRequest = PinRequest(id: "testId", title: "testTitle", content: "testContent", longitude: 0.0, latitude: 0.0, category: "testCategory", created: "testCreated", userId: "testUserId", urls: ["testUrl"])
+
+        // When
+        let response = await viewModel.loadPin(pin: pinRequest)
+
+        // Then
+        XCTAssertNotNil(response, "loadPin should return a valid PinResponse")
+        XCTAssertEqual(response.images.count, pinRequest.urls.count, "The number of images should match the number of URLs in the pin request")
+    }
+
+    func testFetchUserInfo() async {
+        // Given
+        let mockFirestorageService = MockFirestorageService()
+        let mockUserService = MockUserService()
+        let useCase = MainUseCase(firestorageService: mockFirestorageService, userService: mockUserService)
+
+        // When
+        let userInfo = await useCase.fetchUserInfo()
+
+        // Then
+        XCTAssertEqual(userInfo, mockUserService.mockUser, "fetchUserInfo should return the user from the mock service")
+    }
+
+    func testSetCreateViewIsPresented() {
+        // Given
+        let mockFirestorageService = MockFirestorageService()
+        let mockUserService = MockUserService()
+        let useCase = MainUseCase(firestorageService: mockFirestorageService, userService: mockUserService)
+        let viewModel = MainViewModel(mainUseCase: useCase)
+
+        // When
+        viewModel.setCreateViewIsPresented(isPresented: true)
+
+        // Then
+        XCTAssertTrue(viewModel.getCreateViewIsPresented(), "createViewIsPresented should be true")
+    }
+
+    func testGetCreateViewIsPresented() {
+        // Given
+        let mockFirestorageService = MockFirestorageService()
+        let mockUserService = MockUserService()
+        let useCase = MainUseCase(firestorageService: mockFirestorageService, userService: mockUserService)
+        let viewModel = MainViewModel(mainUseCase: useCase)
+        viewModel.setCreateViewIsPresented(isPresented: true)
+
+        // When
+        let isPresented = viewModel.getCreateViewIsPresented()
+
+        // Then
+        XCTAssertTrue(isPresented, "getCreateViewIsPresented should return true")
+    }
+
+    func testToggleCreateViewIsPresented() {
+        // Given
+        let mockFirestorageService = MockFirestorageService()
+        let mockUserService = MockUserService()
+        let useCase = MainUseCase(firestorageService: mockFirestorageService, userService: mockUserService)
+        let viewModel = MainViewModel(mainUseCase: useCase)
+        let initialValue = viewModel.getCreateViewIsPresented()
+
+        // When
+        viewModel.toggleCreateViewIsPresented()
+
+        // Then
+        XCTAssertNotEqual(viewModel.getCreateViewIsPresented(), initialValue, "createViewIsPresented should be toggled")
     }
 }
