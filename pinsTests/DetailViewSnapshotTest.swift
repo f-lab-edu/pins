c6605f7c09b4b6a3c67d1d64af0588201c0764bc
MojitoBar <wnehdtjr5@gmail.com>
2023-12-21 22:58:23 +0900

    테스트 코드 수정

--- pinsTests/DetailViewSnapshotTest.swift
+++ pinsTests/DetailViewSnapshotTest.swift
@@ -10,14 +10,14 @@ final class DetailViewSnapshotTest: FBSnapshotTestCase {
     func test_DetailViewController_WithNoImage() {
         let detailVC = DetailViewController()
         let pinRequest = PinRequest(id: "testId", title: "test title", content: "test content", longitude: 0.0, latitude: 0.0, category: "산책", created: "", userId: "testUser")
-        detailVC.setPin(pin: PinResponse(pin: pinRequest, images: [], id: "testUser", name: "user1", age: 20, description: "desc", profile: UIImage.test))
+        detailVC.setPin(pin: PinResponse(pin: pinRequest, images: [], id: "testUser", name: "user1", age: 20, description: "desc", profile: UIImage(asset: .testImage)))
         FBSnapshotVerifyViewController(detailVC)
     }
     
     func test_DetailViewController_WithImage() {
         let detailVC = DetailViewController()
         let pinRequest = PinRequest(id: "testId", title: "test title", content: "test content", longitude: 0.0, latitude: 0.0, category: "산책", created: "", userId: "testUser")
-        detailVC.setPin(pin: PinResponse(pin: pinRequest, images: [UIImage.test], id: "testUser", name: "user1", age: 20, description: "desc", profile: UIImage.test))
+        detailVC.setPin(pin: PinResponse(pin: pinRequest, images: [UIImage(asset: .testImage)], id: "testUser", name: "user1", age: 20, description: "desc", profile: UIImage(asset: .testImage)))
         FBSnapshotVerifyViewController(detailVC)
     }
 }
